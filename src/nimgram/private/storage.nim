f7f8a0ea5998c23a47935f4b7323e68a85d1a1e5
dadadani <dadadani2134@gmail.com>
2021-10-18 13:01:43 +0200

    Use `before install` on Nimble file

--- src/nimgram/private/storage.nim
+++ src/nimgram/private/storage.nim
@@ -21,7 +21,7 @@ when compileOption("threads"):
     import db_sqlite
     import base64
 import utils/binmanager
-import shared 
+import shared
 
 type NimgramStorageConfig* = ref object of RootObj
 
@@ -37,12 +37,12 @@ type NimgramStorage* = ref object of RootObj
 
 ## Storage using ram: Saves information on ram
 ## This might be helpful when creating scalable instances saving disk usage
-## with the disavantage of consuming more ram over time. 
+## with the disavantage of consuming more ram over time.
 ## If you want to clear saved information, use the clearCache method
 type NimgramStorageRam* = ref object of NimgramStorage
     binFileName: string
     peerStorage: Table[int64, StoragePeer]
-    
+
 when compileOption("threads"):
     ## Storage using sqlite: Saves information on file database (sqlite)
     ## Saved information are permanent, since are written on disk.
@@ -54,35 +54,39 @@ when compileOption("threads"):
         sqliteInstance: SqlManager
 
 
-proc init*(self: NimgramStorageRam, config: NimgramStorageConfig) = 
+proc init*(self: NimgramStorageRam, config: NimgramStorageConfig) =
     doAssert config of NimgramStorageConfigRam, "Expecting config of type NimgramStorageRam"
 
     var ramConfig = config.NimgramStorageConfigRam
     self.binFileName = ramConfig.binfilename
 
-proc writeSessionsInfo*(self: NimgramStorageRam, dcOptions: Table[int, DcOption]) {.async.} = 
+proc writeSessionsInfo*(self: NimgramStorageRam, dcOptions: Table[int,
+        DcOption]) {.async.} =
     await writeBin(self.binFileName, dcOptions)
 
 proc clearCache*(self: NimgramStorageRam) = self.peerStorage.clear()
 
-proc getSessionsInfo*(self: NimgramStorageRam): Future[Table[int, DcOption]] {.async.} = 
+proc getSessionsInfo*(self: NimgramStorageRam): Future[Table[int,
+        DcOption]] {.async.} =
     return await loadBin(self.binFileName)
 
 proc addPeer*(self: NimgramStorageRam, user: StoragePeer) {.async.} =
     self.peerStorage[user.peerID] = user
 
-proc getPeer*(self: NimgramStorageRam, user: int64): Future[StoragePeer] {.async.} = 
+proc getPeer*(self: NimgramStorageRam, user: int64): Future[
+        StoragePeer] {.async.} =
     return self.peerStorage[user]
 
 when compileOption("threads"):
-    proc init*(self: NimgramStorageSqlite, config: NimgramStorageConfig) = 
+    proc init*(self: NimgramStorageSqlite, config: NimgramStorageConfig) =
         doAssert config of NimgramStorageConfigSqlite, "Expecting config of type NimgramStorageSqlite"
 
         var sqliteConfig = config.NimgramStorageConfigSqlite
         self.disableCache = sqliteConfig.disableCache
         self.sqliteInstance = initSqlite(sqliteConfig.filename)
 
-    proc writeSessionsInfo*(self: NimgramStorageSqlite, dcOptions: Table[int, DcOption]) {.async.} =
+    proc writeSessionsInfo*(self: NimgramStorageSqlite, dcOptions: Table[int,
+            DcOption]) {.async.} =
         await self.sqliteInstance.exec(sql("DELETE from dcoptions;"))
         var query = ""
         for _, key in dcOptions:
@@ -94,24 +98,29 @@ when compileOption("threads"):
                 isMain = "1"
             var authKey = encode(key.authKey)
             var salt = encode(key.salt)
-            await self.sqliteInstance.exec(sql(&"INSERT INTO dcoptions (number, isAuthorized, isMain, authKey, salt) VALUES ({key.number}, {isAuthorized}, {isMain}, '{authKey}', '{salt}');"))
+            await self.sqliteInstance.exec(sql(
+                    &"INSERT INTO dcoptions (number, isAuthorized, isMain, authKey, salt) VALUES ({key.number}, {isAuthorized}, {isMain}, '{authKey}', '{salt}');"))
 
     proc addPeer*(self: NimgramStorageSqlite, user: StoragePeer) {.async.} =
-        await self.sqliteInstance.exec(sql(&"INSERT INTO peerdata(id, access_hash) VALUES({user.peerID}, '{user.accessHash}') ON CONFLICT(id) DO UPDATE SET access_hash='{user.accessHash}';"))
-        if not self.disableCache: 
+        await self.sqliteInstance.exec(sql(
+                &"INSERT INTO peerdata(id, access_hash) VALUES({user.peerID}, '{user.accessHash}') ON CONFLICT(id) DO UPDATE SET access_hash='{user.accessHash}';"))
+        if not self.disableCache:
             #TODO: maybe add cache auto clean if we reach many chats stored?
             self.cache[user.peerID] = user
 
-    proc getPeer*(self: NimgramStorageSqlite, user: int64): Future[StoragePeer] {.async.} =
+    proc getPeer*(self: NimgramStorageSqlite, user: int64): Future[
+            StoragePeer] {.async.} =
         if not self.disableCache and self.cache.contains(user):
             return self.cache[user]
-        var row = await self.sqliteInstance.getRow(sql(&"select access_hash from peerdata where id = {user}"))
+        var row = await self.sqliteInstance.getRow(sql(
+                &"select access_hash from peerdata where id = {user}"))
         return StoragePeer(peerID: user, accessHash: row[0].parseBiggestInt)
 
-    proc clearCache*(self: NimgramStorageSqlite) = 
+    proc clearCache*(self: NimgramStorageSqlite) =
         self.cache.clear()
 
-    proc getSessionsInfo*(self: NimgramStorageSqlite): Future[Table[int, DcOption]] {.async.} =
+    proc getSessionsInfo*(self: NimgramStorageSqlite): Future[Table[int,
+            DcOption]] {.async.} =
         var rows = await self.sqliteInstance.getAllRows(sql("SELECT number, isAuthorized, isMain, authKey, salt FROM dcoptions"))
         for row in rows:
             var number = parseInt(row[0]).uint16
@@ -119,10 +128,12 @@ when compileOption("threads"):
             var isMain = if row[2] == "1": true else: false
             var authKey = cast[seq[uint8]](decode(row[3]))
             var salt = cast[seq[uint8]](decode(row[4]))
-            result[int(number)] = DcOption(number: number, isAuthorized: isAuthorized, isMain: isMain, authKey: authKey, salt: salt)
+            result[int(number)] = DcOption(number: number,
+                    isAuthorized: isAuthorized, isMain: isMain,
+                    authKey: authKey, salt: salt)
 
 
-proc init*(self: NimgramStorage, config: NimgramStorageConfig) = 
+proc init*(self: NimgramStorage, config: NimgramStorageConfig) =
     when compileOption("threads"):
         if self of NimgramStorageSqlite:
             self.NimgramStorageSqlite.init(config)
@@ -132,17 +143,18 @@ proc init*(self: NimgramStorage, config: NimgramStorageConfig) =
         return
     raise newException(CatchableError, "Unable to resolve type")
 
-proc writeSessionsInfo*(self: NimgramStorage, dcOptions: Table[int, DcOption]) {.async.} =
+proc writeSessionsInfo*(self: NimgramStorage, dcOptions: Table[int,
+        DcOption]) {.async.} =
     if self of NimgramStorageRam:
         await self.NimgramStorageRam.writeSessionsInfo(dcOptions)
-        
+
     when compileOption("threads"):
         if self of NimgramStorageSqlite:
             await self.NimgramStorageSqlite.writeSessionsInfo(dcOptions)
-            
 
 
-proc clearCache*(self: NimgramStorage) = 
+
+proc clearCache*(self: NimgramStorage) =
     if self of NimgramStorageRam:
         self.NimgramStorageRam.clearCache()
     when compileOption("threads"):
@@ -150,14 +162,15 @@ proc clearCache*(self: NimgramStorage) =
             self.NimgramStorageSqlite.clearCache()
 
 
-proc getSessionsInfo*(self: NimgramStorage): Future[Table[int, DcOption]] {.async.} = 
-            
+proc getSessionsInfo*(self: NimgramStorage): Future[Table[int,
+        DcOption]] {.async.} =
+
     if self of NimgramStorageRam:
         return await self.NimgramStorageRam.getSessionsInfo()
     when compileOption("threads"):
         if self of NimgramStorageSqlite:
             return await self.NimgramStorageSqlite.getSessionsInfo()
-   
+
 
 
 proc addPeer*(self: NimgramStorage, user: StoragePeer) {.async.} =
@@ -167,8 +180,9 @@ proc addPeer*(self: NimgramStorage, user: StoragePeer) {.async.} =
         if self of NimgramStorageSqlite:
             await self.NimgramStorageSqlite.addPeer(user)
 
-proc getPeer*(self: NimgramStorage, user: int64): Future[StoragePeer] {.async.} = 
-    
+proc getPeer*(self: NimgramStorage, user: int64): Future[
+        StoragePeer] {.async.} =
+
     if self of NimgramStorageRam:
         return await self.NimgramStorageRam.getPeer(user)
     when compileOption("threads"):
